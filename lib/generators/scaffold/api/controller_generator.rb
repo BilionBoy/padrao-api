9e484ea7973cd29e1a56ceed5e3cf21a82e665a9
Dhiogo <dhiogoandre26@gmail.com>
2025-02-17 11:16:15 -0400

    Criando generators com: mkdir -p lib/generators/scaffold/api/templates e touch lib/generators/scaffold/api/controller_generator.rb

---
+++ lib/generators/scaffold/api/controller_generator.rb
@@ -0,0 +1,13 @@
+require "rails/generators/rails/scaffold_controller/scaffold_controller_generator"
+
+module Scaffold
+  module Api
+    class ControllerGenerator < Rails::Generators::ScaffoldControllerGenerator
+      source_root File.expand_path("templates", __dir__)
+
+      def create_controller_files
+        template "controller.rb.tt", File.join("app/controllers", class_path, "#{controller_file_name}_controller.rb")
+      end
+    end
+  end
+end
